6caaea21e2667d602ae0aa8578267cd6ef39c0e1
mickey mai <maiweiqi@youzan.com>
2017-09-24 17:18:07 +0800

    状态按钮

--- pages/gamedetail/gamedetail.js
+++ pages/gamedetail/gamedetail.js
@@ -13,37 +13,37 @@ Page({
     introTruncate: true,
     hasEntrance: false,
     nickName: "",
-    now : (new Date()).getTime(),
+    now: (new Date()).getTime(),
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    app.getUserInfo(userInfo=>{
-        console.log(userInfo)
-        this.setData({
-            userInfo,
-            // nickName: userInfo.nickName,
-        })
+    app.getUserInfo(userInfo => {
+      console.log(userInfo)
+      this.setData({
+        userInfo,
+        // nickName: userInfo.nickName,
+      })
     })
     let {
       id
     } = options
-    if(!id && id !== 0) {
+    if (!id && id !== 0) {
       wx.redirectTo({
         url: '../gamelist/gamelist',
       })
     }
     this.gameID = parseInt(id)
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
@@ -57,28 +57,28 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-      this.fetchGameDetail()
+    this.fetchGameDetail()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
@@ -86,19 +86,19 @@ Page({
    */
   onShareAppMessage: function () {
     return {
-        title: this.data.gameDetail.game_title,
+      title: this.data.gameDetail.game_title,
     }
   },
-  fetchGameDetail: function() {
+  fetchGameDetail: function () {
     let that = this
     app.requestWithOpenID({
       url: `/games/${this.gameID}`,
       success: data => {
         let entrances = data.entrances
-        if(entrances.length < 6 && data.judge) {
+        if (entrances.length < 6 && data.judge) {
           entrances = entrances.concat([data.judge])
-          entrances = entrances.reduce((prev, curr)=>{
-            if(prev.filter(item => item.openid == curr.openid).length === 0) {
+          entrances = entrances.reduce((prev, curr) => {
+            if (prev.filter(item => item.openid == curr.openid).length === 0) {
               return prev.concat([curr])
             }
             return prev.slice(0)
@@ -111,7 +111,7 @@ Page({
           })
         })
         wx.setNavigationBarTitle({
-            title: data.game_title,
+          title: data.game_title,
         })
       },
     })
@@ -121,29 +121,29 @@ Page({
       introTruncate: !this.data.introTruncate
     })
   },
-  entranceAction: function() {
+  entranceAction: function () {
     wx.navigateTo({
       url: `../entranceinfo/entranceinfo?id=${this.gameID}`,
     })
   },
 
-  onNickNameChanged: function(e) {
-      this.setData({
-          nickName: e.detail.value,
-      })
-      return e.detail.value
+  onNickNameChanged: function (e) {
+    this.setData({
+      nickName: e.detail.value,
+    })
+    return e.detail.value
   },
-  cancelEntrance: function() {
+  cancelEntrance: function () {
     app.requestWithOpenID({
       url: "/entrances/cancel",
-      method:"POST",
+      method: "POST",
       data: {
         game_id: this.gameID,
       },
-      success: ()=> this.fetchGameDetail()
+      success: () => this.fetchGameDetail()
     })
   },
-  pay: function() {
+  pay: function () {
     let {
       prepay_id
     } = this.data.gameDetail
@@ -152,11 +152,20 @@ Page({
       complete: () => this.fetchGameDetail()
     })
   },
-  getDisplayTime: function(gameDetail) {
-    if(!gameDetail || !gameDetail.start_time) {
+  getDisplayTime: function (gameDetail) {
+    if (!gameDetail || !gameDetail.start_time) {
       return ""
     }
     let dt = new Date(gameDetail.start_time)
-    return `${dt.getFullYear()}年${dt.getMonth()+1}月${dt.getDate()}日 ${dt.getHours()}:${dt.getMinutes()}`
+    return `${dt.getFullYear()}年${dt.getMonth() + 1}月${dt.getDate()}日 ${dt.getHours()}:${dt.getMinutes()}`
+  },
+  cancelGame: function () {
+    app.request({
+      method: 'DELETE',
+      url: `/games/${this.gameID}`,
+      success: () => wx.navigateBack({
+
+      })
+    })
   }
 })
